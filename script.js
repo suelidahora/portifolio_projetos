e330224c1f59ed06844361fbb14ce8c84f146636
Sueli da Hora <sueli-dahora@outlook.com>
2024-11-20 19:09:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ function changeLanguage(lang) {
     const buttons = document.querySelectorAll('.btn-language');
     buttons.forEach(btn => btn.classList.remove('active'));
     document.querySelector(`[onclick="changeLanguage('${lang}')"]`).classList.add('active');
+// Lógica de redirecionamento
+    if (lang === 'en') {
+        window.location.href = 'index_en.html'; // Página em inglês
+    } else {
+        window.location.href = 'index.html'; // Página em português
+    }
 }
